09c8d4c8c71bc7e70528681382dcf6116794d634
Zidan M. Daffa <zidandaff@gmail.com>
2021-05-07 22:42:47 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+// Elements
 const toggleBtn = document.querySelector('.toggle-btn');
 const line1 = document.querySelector('.line-menu:nth-child(1)')
 const line2 = document.querySelector('.line-menu:nth-child(2)')
 const navLinks = document.querySelectorAll('.nav-links a')
-
+const navLinksParent = document.querySelector('.nav-links')
+const mouseCursor = document.querySelector('.cursor')
+const projects = document.querySelectorAll('.project-item a')
 
 // Navbar animation GSAP
 const navAnim = gsap.timeline({paused: true}); 
@@ -16,29 +19,91 @@ toggleBtn.addEventListener('click', ()=> {
     navAnim.reversed() ? navAnim.play() : navAnim.reverse()
 })
 
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>{
+        navAnim.reverse()
+    })
+    link.addEventListener('mouseover', () => {
+        mouseCursor.style.transition = "all 0.3s ease"
+    })
+    link.parentElement.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            mouseCursor.style.transition = "transform 0.3s ease"
+        }, 500);
+    })
+})
+
+// cursor
+window.addEventListener('mousemove', function(e){
+    if(e.target.classList.contains('nav-links-item')){
+        navOnHover(mouseCursor, e.target)
+    }
+    else {
+        followCursor(mouseCursor, e)
+    }
+})
+
+function followCursor(cursor, coordinate){
+    cursor.style.top = coordinate.clientY + "px";
+    cursor.style.left = coordinate.clientX + "px";
+    cursor.style.width = '2rem';
+    cursor.style.height = '2rem';
+    cursor.classList.remove('on-focus');
+}
+
+function navOnHover(cursor, elem){
+    const rect = elem.getBoundingClientRect();
+    cursor.classList.add('on-focus')
+    cursor.style.top = rect.top + "px";
+    cursor.style.left = rect.left + "px";
+    cursor.style.width = rect.width + "px";
+    cursor.style.height = rect.height + "px";
+}
+
 // Hero animation GSAP
 const heroAnim = gsap.timeline( {defaults: {duration: .7}, delay: .8 } );
 const videoMask = CSSRulePlugin.getRule(".video-wrapper::after")
 heroAnim.from(videoMask, {cssRule:{scaleX: 1, duration: 2}})
         .from('.intro-text h1 span', {y: '100%', stagger: .3})
 
-// Project animation
-const boxes = gsap.utils.toArray('.project-img img');
-boxes.forEach(box => {
-gsap.to(box, { 
+// Project animation GSAP
+const projectsThumb = gsap.utils.toArray('.project-thumb a');
+projectsThumb.forEach(project => {
+gsap.to(project, { 
     y: '-30%',
     scrollTrigger: {
         start: 'top center',
-        trigger: box.parentElement,
+        trigger: project.parentElement,
         scrub: true
         }
     })
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', ()=>{
-        navAnim.reverse()
-    })
+projects.forEach( project => {
+    project.addEventListener('mouseover', ()=> mouseCursor.classList.add('active'))
+    project.addEventListener('mouseleave', ()=> mouseCursor.classList.remove('active'))
 })
 
+// About animation GSAP
+const aboutAnim = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#about',
+        start: "top center",
+        scrub: true
+    }
+})
+
+aboutAnim.to('#about .sub-header h1', {y: 50})
+            .to('.profile-pic', {y: -70}, '<')
+            .from('.about-paragraf p', {opacity: 0.6, x: 30}, "<")
+
+// Smooth scroll
 const scroll = new SmoothScroll('a[href*="#"]');
+
+// Progress bar page
+window.addEventListener("scroll", ()=> {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.height = scrolled + "%";
+})
